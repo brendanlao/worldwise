2b0fe809f59446df786aa7efceb744dd1cd0d244
Brendan Lao <brendanlao@Brendans-MacBook-Pro.local>
2024-08-17 22:41:49 -0700

    change router to createBrowserRouter, change not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
 import CityList from "./components/CityList";
 import City from "./components/City";
@@ -10,12 +14,6 @@ import { AuthProvider } from "./contexts/AuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import { lazy, Suspense } from "react";
 
-// import Homepage from "./pages/Homepage";
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import AppLayout from "./pages/AppLayout";
-// import Login from "./pages/Login";
-// import NotFound from "./pages/PageNotFound";
 const Homepage = lazy(() => import("./pages/Homepage"));
 const Product = lazy(() => import("./pages/Product"));
 const Pricing = lazy(() => import("./pages/Pricing"));
@@ -23,38 +21,80 @@ const AppLayout = lazy(() => import("./pages/AppLayout"));
 const Login = lazy(() => import("./pages/Login"));
 const NotFound = lazy(() => import("./pages/PageNotFound"));
 
+const router = createBrowserRouter([
+  { path: "/", element: <Homepage />, errorElement: <NotFound /> },
+  { path: "pricing", element: <Pricing /> },
+  { path: "product", element: <Product /> },
+  { path: "login", element: <Login /> },
+  {
+    path: "app",
+    element: (
+      <ProtectedRoute>
+        <AppLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      { index: true, element: <Navigate replace to="cities" /> },
+      { path: "cities", element: <CityList /> },
+      {
+        path: "cities/:id",
+        element: <City />,
+      },
+      {
+        path: "countries",
+        element: <CountryList />,
+      },
+      {
+        path: "form",
+        element: <Form />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <CitiesProvider>
       <AuthProvider>
-        <BrowserRouter>
-          <Suspense fallback={<SpinnerFullPage />}>
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="pricing" element={<Pricing />} />
-              <Route path="product" element={<Product />} />
-              <Route path="login" element={<Login />} />
-              <Route
-                path="app"
-                element={
-                  <ProtectedRoute>
-                    <AppLayout />
-                  </ProtectedRoute>
-                }
-              >
-                <Route index element={<Navigate replace to="cities" />} />
-                <Route path="cities" element={<CityList />} />
-                <Route path="cities/:id" element={<City />} />
-                <Route path="countries" element={<CountryList />} />
-                <Route path="form" element={<Form />} />
-              </Route>
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage />}>
+          <RouterProvider router={router} />
+        </Suspense>
       </AuthProvider>
     </CitiesProvider>
   );
+
+  //* Without using createBrowserRouter
+  // return (
+  //   <CitiesProvider>
+  //     <AuthProvider>
+  //       <BrowserRouter>
+  //         <Suspense fallback={<SpinnerFullPage />}>
+  //           <Routes>
+  //             <Route path="/" element={<Homepage />} />
+  //             <Route path="pricing" element={<Pricing />} />
+  //             <Route path="product" element={<Product />} />
+  //             <Route path="login" element={<Login />} />
+  //             <Route
+  //               path="app"
+  //               element={
+  //                 <ProtectedRoute>
+  //                   <AppLayout />
+  //                 </ProtectedRoute>
+  //               }
+  //             >
+  //               <Route index element={<Navigate replace to="cities" />} />
+  //               <Route path="cities" element={<CityList />} />
+  //               <Route path="cities/:id" element={<City />} />
+  //               <Route path="countries" element={<CountryList />} />
+  //               <Route path="form" element={<Form />} />
+  //             </Route>
+  //             <Route path="*" element={<NotFound />} />
+  //           </Routes>
+  //         </Suspense>
+  //       </BrowserRouter>
+  //     </AuthProvider>
+  //   </CitiesProvider>
+  // );
 }
 
 export default App;
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,16 @@
+import PageNav from "../components/PageNav";
+import styles from "./PageNotFound.module.css";
 export default function PageNotFound() {
   return (
-    <div>
-      <h1 style={{ color: "black" }}>Page not found 😢</h1>
+    <div className={styles.container}>
+      <PageNav />
+      <div className={styles.textContainer}>
+        <h1 className={styles.error}>Oops!</h1>
+        <p className={styles.text}>Sorry, an unexpected error has occured.</p>
+        <p className={styles.text}>
+          <em>Page Not Found</em>
+        </p>
+      </div>
     </div>
   );
 }
